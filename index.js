852a627d889c96d7d78d57dc23edc75a328f8338
Erik Grinn <grinn.erik@gmail.com>
2026-02-06 17:14:48 -0500

    css

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 const server = http.createServer((req, res) => {
+  // for CSS
+  if (req.url === "/styles.css") {
+    const cssPath = path.join(__dirname, "styles.css");
+    fs.readFile(cssPath, (err, content) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(content);
+      }
+    });
+    return;
+  }
+
+  // for HTML
   let filePath = "index.html";
 
   if (req.url === "/") {
     filePath = "index.html";
   } else if (req.url === "/about") {
     filePath = "about.html";
-  } else if (req.url === "/contact-me") {
-    filePath = "contact-me.html";
+  } else if (req.url === "/contact") {
+    filePath = "contact.html";
   } else {
     filePath = "404.html";
   }
@@ -19,6 +35,7 @@ const server = http.createServer((req, res) => {
 
   fs.readFile(fullPath, (err, content) => {
     if (err) {
+      // if there is no 404.html file
       res.writeHead(404, { "Content-Type": "text/html" });
       res.end("404 Not Found");
     } else {
