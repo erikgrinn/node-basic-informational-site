2b064130fe2da8035e443baf6e3f80c7f124f237
Erik Grinn <grinn.erik@gmail.com>
2026-02-19 14:21:48 -0500

    ejs

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // app.js
+const path = require("node:path");
 const express = require("express");
 const app = express();
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
 
-app.use(express.static(__dirname));
+// this is serving the static index HTML file,
+// making the explicit sendfile redundant...
+// app.use(express.static(__dirname));
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
+
+// views portion
+
+// below placed in indexRouter
+// app.get("/", (req, res) => {
+//   res.render("index", { message: "EJS rocks!" });
+// });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,16 @@
-const path = require("path");
+const path = require("node:path");
 const { Router } = require("express");
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "index.html"));
+  res.render("index", { message: "EJS rocks!" });
 });
+
+// this is not run because of above, and no next() use in route handler
+// indexRouter.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "index.html"));
+// });
 
 indexRouter.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "about.html"));
